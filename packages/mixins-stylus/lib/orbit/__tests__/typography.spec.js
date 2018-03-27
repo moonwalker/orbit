c7b496d861465f78d3ec55bfdd905c197c22ec00
Vio vio@moonwalker.tech <vio@beanon.com>
2018-03-27 15:16:03 +0200

    Add type scale helper

---
+++ packages/mixins-stylus/lib/orbit/__tests__/typography.spec.js
@@ -0,0 +1,11 @@
+const assert = require('../../utils/assert');
+
+describe('Typography', () => {
+  test('type-scale', (done) => {
+    assert(
+      __dirname,
+      'typography-type-scale',
+      done,
+    );
+  });
+});
