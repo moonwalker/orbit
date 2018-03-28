6c2128bee074a083ebe64acbcf10a92595889d8f
Vio vio@moonwalker.tech <vio@beanon.com>
2018-03-28 18:32:12 +0200

    fix(ui-react): Add missing primary / secondary kinds to Button

--- packages/ui-react/src/atoms/button/button.constants.js
+++ packages/ui-react/src/atoms/button/button.constants.js
@@ -1,6 +1,8 @@
 export const UI_NAME = 'ui-button';
 
 export const KIND_DEFAULT = '';
+export const KIND_PRIMARY = 'primary';
+export const KIND_SECONDARY = 'secondary';
 export const KIND_SUCCESS = 'success';
 export const KIND_INFO = 'info';
 export const KIND_WARNING = 'warning';
@@ -8,6 +10,8 @@ export const KIND_DANGER = 'danger';
 
 export const KINDS = [
   KIND_DEFAULT,
+  KIND_PRIMARY,
+  KIND_SECONDARY,
   KIND_SUCCESS,
   KIND_INFO,
   KIND_WARNING,
