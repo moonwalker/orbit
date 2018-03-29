1bfee46121ad99f9cfbd6d1df501370564042bec
Vio vio@moonwalker.tech <vio@beanon.com>
2018-03-29 10:34:55 +0200

    feat(mixins-stylus): Position helpers

---
+++ packages/mixins-stylus/lib/orbit/__tests__/position.spec.js
@@ -0,0 +1,19 @@
+const assert = require('../../utils/assert');
+
+describe('Position', () => {
+  test('position', (done) => {
+    assert(
+      __dirname,
+      'position',
+      done,
+    );
+  });
+
+  test('absolute', (done) => {
+    assert(
+      __dirname,
+      'position-absolute',
+      done,
+    );
+  });
+});
