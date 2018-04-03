9c92ee29d52fdfcbcc25dba30bdf052c19e36d64
Vio vio@moonwalker.tech <vio@beanon.com>
2018-04-03 15:40:52 +0200

    feat(ui-react): Select component

--- packages/ui-react/src/index.js
+++ packages/ui-react/src/index.js
@@ -1,5 +1,6 @@
 export { Button } from './atoms/button/button';
 export { Input } from './atoms/input/input';
 export { Icon } from './atoms/icon/icon';
+export { Select } from './atoms/select/select';
 export { Textarea } from './atoms/textarea/textarea';
 export { Spinner } from './atoms/spinner/spinner';
