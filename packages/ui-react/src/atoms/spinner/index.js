ec56254a2eec1b87bcd50f0bce32dd724ad06101
Vio vio@moonwalker.tech <vio@beanon.com>
2018-04-10 14:54:32 +0200

    fix(ui-react): Export prefixed constants for Spinner'

--- packages/ui-react/src/atoms/spinner/index.js
+++ packages/ui-react/src/atoms/spinner/index.js
@@ -1,8 +1,8 @@
 export * from './spinner';
 
 export {
-  SIZE_SMALL,
-  SIZE_MEDIUM,
-  SIZE_LARGE,
-  SIZES,
+  SIZE_SMALL as SPINNER_SIZE_SMALL,
+  SIZE_MEDIUM as SPINNER_SIZE_MEDIUM,
+  SIZE_LARGE as SPINNER_SIZE_LARGE,
+  SIZES as SPINNER_SIZES,
 } from './spinner.constants';
