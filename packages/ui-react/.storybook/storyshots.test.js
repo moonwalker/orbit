7b8687f12f275e754ae301a2947b47a1ceac16c1
Vio vio@moonwalker.tech <vio@beanon.com>
2018-05-18 11:15:34 +0200

    build(ui-react): Do not snapshot with info

--- packages/ui-react/.storybook/storyshots.test.js
+++ packages/ui-react/.storybook/storyshots.test.js
@@ -1,3 +1,8 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
+jest.mock('@storybook/addon-info', () => ({
+  withInfo: () => storyFn => storyFn,
+  setDefaults: () => {},
+}))
+
 initStoryshots();
