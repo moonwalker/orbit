d7fe3f9c5a08c8a8fd67b754f3366a1861c59f98
Vio vio@moonwalker.tech <vio@beanon.com>
2019-05-21 09:27:45 +0200

    build(ui-react): Fix storybook config

--- packages/ui-react/.storybook/webpack.config.js
+++ packages/ui-react/.storybook/webpack.config.js
@@ -5,76 +5,86 @@ const autoprefixer = require('autoprefixer');
 const orbitUI = require('@moonwalker/orbit-ui-stylus');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = storybookConfig => webpackMerge(storybookConfig, {
-  resolve: {
-    modules: [
-      resolve(__dirname, '..', 'node_modules'),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(css|styl)$/,
-        enforce: 'post',
-        use: process.env.NODE_ENV === 'production'
+module.exports = ({ config }) => {
+  const { rules } = config.modules;
+
+  // Remove storybook css rule
+  config.module.rules = [
+    ...rules.slice(0, 2),
+    ...rules.slice(3)
+  ];
+
+  return webpackMerge(config, {
+    resolve: {
+      modules: [
+        resolve(__dirname, '..', 'node_modules'),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(css|styl)$/,
+          enforce: 'post',
+          use: process.env.NODE_ENV === 'production'
           ? MiniCssExtractPlugin.loader
           : 'style-loader'
-      },
-      {
-        test: /\.(css|styl)$/,
-        oneOf: [
-          {
-            test: /modules\.(css|styl)$/,
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true,
-                  localIdentName: '[path]-[name]__[local]',
+        },
+        {
+          test: /\.(css|styl)$/,
+          oneOf: [
+            {
+              test: /modules\.(css|styl)$/,
+              use: [
+                {
+                  loader: 'css-loader',
+                  options: {
+                    modules: true,
+                    localIdentName: '[path]-[name]__[local]',
+                  },
                 },
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: true,
-                  plugins: [
-                    autoprefixer,
-                  ],
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    sourceMap: true,
+                    plugins: [
+                      autoprefixer,
+                    ],
+                  },
                 },
-              },
-            ]
-          },
-          {
-            use: [
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: true,
-                  plugins: [
-                    autoprefixer,
-                  ],
+              ]
+            },
+            {
+              use: [
+                'css-loader',
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    sourceMap: true,
+                    plugins: [
+                      autoprefixer,
+                    ],
+                  },
                 },
-              },
-            ],
-          },
-        ]
-      },
-      {
-        test: /\.styl$/,
-        enforce: 'pre',
-        loader: 'stylus-loader',
-        options: {
-          use: orbitUI(),
+              ],
+            },
+          ]
+        },
+        {
+          test: /\.styl$/,
+          enforce: 'pre',
+          loader: 'stylus-loader',
+          options: {
+            use: orbitUI(),
+          }
         }
-      }
+      ],
+    },
+    plugins: [
+      ...(process.env.NODE_ENV === 'production' ? [
+        new MiniCssExtractPlugin({
+          filename: '[name].css'
+        }),
+      ] : [])
     ],
-  },
-  plugins: [
-    ...(process.env.NODE_ENV === 'production' ? [
-      new MiniCssExtractPlugin({
-        filename: '[name].css'
-      }),
-    ] : [])
-  ],
-});
+  });
+};
