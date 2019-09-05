3160793993753bb9097fef9185c80e57b8cae2e2
Vio vio@moonwalker.tech <vio@beanon.com>
2019-09-05 17:04:10 +0200

    feat(mixins-stylus): Add support for CSS variables

--- packages/mixins-stylus/lib/orbit/__tests__/utils.spec.js
+++ packages/mixins-stylus/lib/orbit/__tests__/utils.spec.js
@@ -4,4 +4,12 @@ describe('Utils', () => {
   test('calc', (done) => {
     assert(__dirname, 'utils-calc', done);
   });
+
+  test('var', (done) => {
+    assert(
+      __dirname,
+      'utils-var',
+      done,
+    );
+  });
 });
