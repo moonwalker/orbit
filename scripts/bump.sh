2010d7d5d6da62f3991cf133604253bf479eaf46
Vio vio@moonwalker.tech <vio@beanon.com>
2019-09-16 11:21:41 +0200

    FIXUP

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -6,9 +6,9 @@ PRERELEASE_ARG=""
 # IF diff than master, release beta
 if [ "$BRANCH" != "master" ]
 then
-  PRERELEASE_ARG="prepatch --preid beta --force-publish --message 'DROP - v%s\n[skip ci]'"
+  PRERELEASE_ARG="prepatch --force-publish --message \"DROP - v%s\n[skip ci]\""
 else
-	PRERELEASE_ARG="--conventional-commits --message ':package: release %s\n[ci skip]'"
+	PRERELEASE_ARG="--conventional-commits --message \":package: release %s\n[ci skip]\""
 fi
 
 RELEASE_ARGS="${PRERELEASE_ARG} ${@}"
