97a1b5eeb7e3a2b4713bc043402bf2a12d31f8e8
Vio vio@moonwalker.tech <vio@beanon.com>
2019-09-16 09:51:14 +0200

    build: Setup release flow

--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git config user.email "ci-build@moonwalker.tech"
-git config user.name "ci-build"
-
 mkdir -p ~/.ssh
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
 cat >> ~/.ssh/config << EOF
 StrictHostKeyChecking no
 EOF
+
+# NPM
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/.npmrc
