edbf3544c1e34404a894aa857aa2c580651c1990
Vio vio@moonwalker.tech <vio@beanon.com>
2019-09-16 11:58:00 +0200

    fixup

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -6,7 +6,7 @@ PRERELEASE_ARG=""
 # IF diff than master, release beta
 if [ "$BRANCH" != "master" ]
 then
-  PRERELEASE_ARG="--force-publish --message \"DROP v%s\n[skip ci]\""
+  PRERELEASE_ARG="prerelease --force-publish --message \"DROP v%s\n[skip ci]\""
 else
 	PRERELEASE_ARG="--conventional-commits --message \":package: release %s\n[ci skip]\""
 fi
@@ -15,4 +15,4 @@ RELEASE_ARGS="${PRERELEASE_ARG} ${@}"
 
 echo "Running lerna version with '${RELEASE_ARGS}'."
 
-./node_modules/.bin/lerna version --yes $RELEASE_ARGS
+./node_modules/.bin/lerna version $RELEASE_ARGS
