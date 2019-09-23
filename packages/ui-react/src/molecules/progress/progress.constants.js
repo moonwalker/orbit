a1a03819525d688f49a069cc767531edbfe0fcbb
Vio vio@moonwalker.tech <vio@beanon.com>
2019-09-23 14:34:56 +0200

    feat(ui-react): Progress component

---
+++ packages/ui-react/src/molecules/progress/progress.constants.js
@@ -0,0 +1,19 @@
+export const UI_NAME = 'ui-progress';
+
+export const KIND_DEFAULT = 'default';
+export const KIND_PRIMARY = 'primary';
+export const KIND_SECONDARY = 'secondary';
+export const KIND_SUCCESS = 'success';
+export const KIND_INFO = 'info';
+export const KIND_WARNING = 'warning';
+export const KIND_DANGER = 'danger';
+
+export const KINDS = [
+  KIND_DEFAULT,
+  KIND_PRIMARY,
+  KIND_SECONDARY,
+  KIND_SUCCESS,
+  KIND_INFO,
+  KIND_WARNING,
+  KIND_DANGER
+];
