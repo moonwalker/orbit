ad67506dd3bc12fa89ed2fb05a328b899332c765
Vio vio@moonwalker.tech <vio@beanon.com>
2021-12-22 11:59:07 +0100

    test: Use component specific snapshot

--- packages/ui-react/.storybook/storyshots.test.js
+++ packages/ui-react/.storybook/storyshots.test.js
@@ -1,3 +1,21 @@
-import initStoryshots from '@storybook/addon-storyshots';
+import initStoryshots, { Stories2SnapsConverter } from '@storybook/addon-storyshots';
+import { render } from '@testing-library/react';
 
-initStoryshots();
+initStoryshots({
+  asyncJest: true,
+  test: ({ story, context, done }) => {
+    const converter = new Stories2SnapsConverter();
+    const snapshotFilename = converter.getSnapshotFileName(context);
+    const storyElement = story.render();
+
+    const { container } = render(storyElement);
+
+    setTimeout(() => {
+      if (snapshotFilename) {
+        expect(container.firstChild).toMatchSpecificSnapshot(snapshotFilename);
+      }
+
+      done();
+    }, 1);
+  }
+});
